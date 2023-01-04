e5a6be8de885c9f79dc883abb95f714abd611a26
carsonadair <carsonadairphoto@gmail.com>
2023-01-04 17:33:57 -0500

    made the tasks show up from local storage saves

--- script.js
+++ script.js
@@ -7,16 +7,36 @@ $(function () {
   var saveButtonEl = $('.saveBtn');
 
     saveButtonEl.on('click', function(){
-    console.log(this)
     var currentHour = this.dataset.hour;
-    console.log(currentHour);
-
     var task = $(`#text-${currentHour}`)[0].value;
-    console.log(task)
-
     localStorage.setItem(`key-${currentHour}`, task);
-  })
+    }
+  )
+
+  // Displays saved tasks from local storage in the text boxes for the corresponding hours.
+  savedTask9 = localStorage.getItem(`key-9`)
+  $(`#text-9`)[0].value = savedTask9
+  savedTask10 = localStorage.getItem(`key-10`)
+  $(`#text-10`)[0].value = savedTask10
+  savedTask11 = localStorage.getItem(`key-11`)
+  $(`#text-11`)[0].value = savedTask11
+  savedTask12 = localStorage.getItem(`key-12`)
+  $(`#text-12`)[0].value = savedTask12
+  savedTask13 = localStorage.getItem(`key-13`)
+  $(`#text-13`)[0].value = savedTask13
+  savedTask14 = localStorage.getItem(`key-14`)
+  $(`#text-14`)[0].value = savedTask14
+  savedTask15 = localStorage.getItem(`key-15`)
+  $(`#text-15`)[0].value = savedTask15
+  savedTask16 = localStorage.getItem(`key-16`)
+  $(`#text-16`)[0].value = savedTask16
+  savedTask17 = localStorage.getItem(`key-17`)
+  $(`#text-17`)[0].value = savedTask17
+
   
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
